5a8a93c5b392697348530bd38d4798ce23438bfe
STUST <STUST@STUST-34>
2023-11-14 13:50:08 +0800

    新增toolbar

--- 1114/04.xaml.cs
+++ 1114/04.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace _1114
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// _04.xaml 的互動邏輯
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class _04 : Window
     {
-        public MainWindow()
+        public _04()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            _04 _04 = new _04();
-            _04.Show();
         }
     }
 }
