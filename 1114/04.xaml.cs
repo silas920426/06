9a2b973326d0206c59dadc96825f1ab7d272eb4d
silas920426 <90045542+silas920426@users.noreply.github.com>
2023-12-04 23:10:59 +0800

    final

--- 1114/04.xaml.cs
+++ 1114/04.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Xceed.Wpf.Toolkit;
 
 namespace _1114
 {
@@ -19,9 +22,20 @@ namespace _1114
     /// </summary>
     public partial class _04 : Window
     {
+        Color fontcolor = Colors.Black;
+        
         public _04()
         {
             InitializeComponent();
+            fontColorPicker.SelectedColor = fontcolor;
+            foreach(var fontFamily in Fonts.SystemFontFamilies) 
+            {
+                fontFamilyComboBox.Items.Add(fontFamily.Source);
+            }
+            fontFamilyComboBox.SelectedIndex = 0;
+
+            fontSizeComboBox.ItemsSource = new List<Double> { 8, 9, 10, 11, 12, 20, 24, 32, 40, 50, 60, 70 };
+            fontSizeComboBox.SelectedIndex = 4;
         }
 
         private void New_Executed(object sender, ExecutedRoutedEventArgs e)
@@ -32,25 +46,106 @@ namespace _1114
 
         private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-            MessageBox.Show("open");
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Rich Text Format (*.rtf)|*.rtf|HTML Files(*.html)|*.html|All files (*.*)|*.*"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                TextRange textRange = new TextRange
+                    (rtbEditor.Document.ContentStart,
+                    rtbEditor.Document.ContentEnd);
+
+                using (FileStream fileStream = new FileStream(openFileDialog.FileName, FileMode.Open))
+                {
+                    textRange.Load(fileStream, DataFormats.Rtf);
+                }
+            }
         }
 
         private void Save_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-            MessageBox.Show("save");
-        }
+            var saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Rich Text Format (*.rtf)|*.rtf|HTML Files(*.html)|*.html|All files (*.*)|*.*"
+            };
 
+            if(saveFileDialog.ShowDialog()==true)
+            {
+                TextRange textRange = new TextRange
+                    (rtbEditor.Document.ContentStart,
+                    rtbEditor.Document.ContentEnd);
+                
+                    using (FileStream fileStream = new FileStream(saveFileDialog.FileName, FileMode.Create))
+                {
+                    textRange.Save(fileStream, DataFormats.Rtf);
+                }
+            }
+
+        }
+    
         private void rtbEditor_SelectionChanged(object sender, RoutedEventArgs e)
         {
+            //依據所選取的文字狀態，同步更新Boldbutton的狀態
+            //粗體
             object property = rtbEditor.Selection.GetPropertyValue (TextElement.FontWeightProperty);
             boldbutton.IsChecked = (property is FontWeight && (FontWeight) property == FontWeights.Bold);
 
+            //斜體 同步更新italicbutton的狀態
             property = rtbEditor.Selection.GetPropertyValue(TextElement.FontStyleProperty);
             italicbutton.IsChecked = (property is FontStyle && (FontStyle) property == FontStyles.Italic);
 
+            //底線 同步更新underlinebutton的狀態
             property = rtbEditor.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
             underlinebutton.IsChecked = (property  != DependencyProperty.UnsetValue && property is TextDecorationCollection decorations &&
-                decorations.Contains(TextDecorations.Underline[0]));
+            decorations.Contains(TextDecorations.Underline[0]));
+
+            //依據所選取文字的字體大小，同步更新fontsizecombox的狀態
+            property = rtbEditor.Selection.GetPropertyValue(TextElement.FontSizeProperty);
+            fontSizeComboBox.SelectedItem = property;
+
+            //依據所選取文字的字型，同步更新fontfamilycombox的狀態
+            property = rtbEditor.Selection.GetPropertyValue(TextElement.FontFamilyProperty);
+            fontFamilyComboBox.SelectedItem = property;
+
+            //依據所選取文字的字體顏色，同步更新fontfamilycombox的狀態
+            SolidColorBrush? forgroundProperty = rtbEditor.Selection.GetPropertyValue(TextElement.ForegroundProperty) as SolidColorBrush;
+            if(forgroundProperty != null)
+            {
+                fontColorPicker.SelectedColor = forgroundProperty.Color;
+            } 
+        }
+
+        private void fontFamilyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        { 
+               rtbEditor.Selection.ApplyPropertyValue(TextElement.FontFamilyProperty, fontFamilyComboBox.SelectedItem);
+            
+        }
+
+        private void fontSizeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+                rtbEditor.Selection.ApplyPropertyValue(TextElement.FontSizeProperty, fontSizeComboBox.SelectedItem);
+            
+        }
+        private void fontColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            fontcolor = (Color)fontColorPicker.SelectedColor;
+            SolidColorBrush fontBrush = new SolidColorBrush(fontcolor);
+            rtbEditor.Selection.ApplyPropertyValue(TextElement.ForegroundProperty, fontBrush);
+
+        }
+
+        private void clearbutton_Click(object sender, RoutedEventArgs e)
+        {
+            rtbEditor.Document.Blocks.Clear();
+        }
+        private void rtbColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            Color selectedColor = (Color)rtbColorPicker.SelectedColor;
+            SolidColorBrush backBrush = new SolidColorBrush(selectedColor);
+            rtbEditor.Selection.ApplyPropertyValue(Control.BackgroundProperty, backBrush);
         }
     }
 }
